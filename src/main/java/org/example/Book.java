eae8f9c0b388d31761ed55cfacf73854742d2913
anastasia.maidacenco <maidacenco100@gmail.com>
2023-02-15 19:53:59 +0200

    Create class Book

---
+++ src/main/java/org/example/Book.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class Book {
+    private String title;
+    private String author;
+    private int numPages;
+    private String publisher;
+
+    // Constructor
+    public Book(String title, String author, int numPages, String publisher) {
+        this.title = title;
+        this.author = author;
+        this.numPages = numPages;
+        this.publisher = publisher;
+    }
+
+    // Getters and setters
+    // ...
+}
