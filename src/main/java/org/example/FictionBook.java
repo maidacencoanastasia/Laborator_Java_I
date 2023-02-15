982c3b14997608fea83212cbed2efb6353c12820
anastasia.maidacenco <maidacenco100@gmail.com>
2023-02-15 20:16:22 +0200

    Create and implement Interface

--- src/main/java/org/example/FictionBook.java
+++ src/main/java/org/example/FictionBook.java
@@ -1,7 +1,8 @@
 package org.example;
 
-public class FictionBook extends Book{
+public class FictionBook extends Book  implements Readable{
     private String genre;
+    private String title;
 
     // Constructor
     public FictionBook(String title, String author, int numPages, String publisher, String genre) {
@@ -10,6 +11,29 @@ public class FictionBook extends Book{
     }
 
     // Getters and setters
-    // ...
+
+    public String getGenre() {
+        return genre;
+    }
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    // Implementarea metodelor din interfață
+    public void open() {
+        System.out.println("Opening fiction book: " + getTitle());
+    }
+
+    public void close() {
+        System.out.println("Closing fiction book: " + getTitle());
+    }
+
+    public void turnPage() {
+        System.out.println("Turning page of fiction book: " + getTitle());
+    }
+
 
 }
---
+++ src/main/java/org/example/Readable.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public interface Readable {
+    public void open();
+    public void close();
+    public void turnPage();
+}
