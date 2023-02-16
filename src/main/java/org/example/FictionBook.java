733e67b8afa4297ce9cb416d450146ce8a88e389
Anastasia <maidacenco100@gmail.com>
2023-02-16 18:44:31 +0200

    Implement

--- src/main/java/org/example/FictionBook.java
+++ src/main/java/org/example/FictionBook.java
@@ -25,4 +25,8 @@ public class FictionBook extends Book implements Readable {
     public double getPrice() {
         return getNumPages() * 0.02; // prețul este de 2 bani pe pagină
     }
+
+    public String getGenre() {
+        return genre;
+    }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,7 +7,7 @@ public class Main {
         Book book = new Book("The Great Gatsby", "F. Scott Fitzgerald", 218, "Scribner") {
             @Override
             public double getPrice() {
-                return 0;
+                return 20;
             }
         };
 
@@ -17,5 +17,17 @@ public class Main {
         System.out.println("Number of pages: " + book.getNumPages());
         System.out.println("Publisher: " + book.getPublisher());
         System.out.println("Price: $" + book.getPrice());
+
+        // Create a new FictionBook object
+        FictionBook fbook = new FictionBook("451° Fahrenheit", "Ray Bradbury", 518, "Scribner", "American fiction");
+
+        // Use the methods of the FictionBook object
+        System.out.println("Title: " + fbook.getTitle());
+        System.out.println("Author: " + fbook.getAuthor());
+        System.out.println("Number of pages: " + fbook.getNumPages());
+        System.out.println("Publisher: " + fbook.getPublisher());
+        System.out.println("Price: $" + fbook.getPrice());
+        System.out.println("Genre: " + fbook.getGenre());
+        fbook.open();
     }
 }
