8a2665a10737cf6910799762c196fe83b0183c17
Anastasia <maidacenco100@gmail.com>
2023-02-16 18:33:46 +0200

    Create a new Book object with the title "The Great Gatsby", author "F. Scott Fitzgerald", number of pages 218, and publisher "Scribner". Then we call the getTitle(), getAuthor(), getNumPages(), getPublisher(), and getPrice() methods on the book object to get information about the book and print it to the console.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,19 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Maidacenco Anastasia 15.02.2023!");
+        // Create a new Book object
+        Book book = new Book("The Great Gatsby", "F. Scott Fitzgerald", 218, "Scribner") {
+            @Override
+            public double getPrice() {
+                return 0;
+            }
+        };
+
+        // Use the methods of the Book object
+        System.out.println("Title: " + book.getTitle());
+        System.out.println("Author: " + book.getAuthor());
+        System.out.println("Number of pages: " + book.getNumPages());
+        System.out.println("Publisher: " + book.getPublisher());
+        System.out.println("Price: $" + book.getPrice());
     }
 }
